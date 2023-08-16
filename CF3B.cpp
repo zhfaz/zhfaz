01fdee1019053695c8d172f2e68596dd49f4abf2
zhfaz <103801109+zhfaz@users.noreply.github.com>
2023-08-16 19:58:25 +0800

    Add files via upload

---
+++ CF3B.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<vector>
+#define MAXN (int)1e5
+using namespace std;
+int f[MAXN+1],c[MAXN+1],w[MAXN+1];
+vector<int> pre[MAXN];
+int main()
+{
+    int n,v,node;
+    scanf("%d %d",&n,&v);
+    for(int i=1;i<=n;i++)
+    {
+        int n,v;
+    }
+    return 0;
+}
